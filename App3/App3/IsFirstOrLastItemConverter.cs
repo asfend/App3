54d2144fd7b9e5f0f9876f32cbde14a4103b42ae
DESKTOP-N17J10J\Asfend <asfend@smashcloudmedia.com>
2016-12-06 15:13:23 +0500

    .

---
+++ App3/App3/IsFirstOrLastItemConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public class IsFirstOrLastItemConverter : IValueConverter
+    {
+
+        #region IValueConverter implementation
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool && (bool)value == true)
+            {
+
+                return 20;
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
